ac47b0b7afad5955d752237ebc631d5f4fa9e8e7
Chip Newman <chip594@gmail.com>
2019-01-13 10:40:45 -0600

    build Kivy foundation

---
+++ src/combine_txt.py
@@ -0,0 +1,28 @@
+# combine_txt.py
+
+import kivy
+kivy.require('1.9.0')
+
+from kivy.core.window import Window
+Window.size = (300, 600)
+
+import os
+from os.path import abspath, relpath
+from pathlib import Path
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+output_file = open('output.txt', 'w')
+
+class Combine_txt_GridLayout(GridLayout):
+    pass
+
+class Combine_txtApp(App):
+    def build(self):
+        return Combine_txt_GridLayout()
+
+ct_app = Combine_txtApp()
+ct_app.run()
+
+output_file.close()
