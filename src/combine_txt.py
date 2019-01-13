9c7afc89c81f4db8c4496163f9e58fdc894fa418
Chip Newman <chip594@gmail.com>
2019-01-13 11:48:34 -0600

    got the pathing to work

--- src/combine_txt.py
+++ src/combine_txt.py
@@ -15,6 +15,24 @@ from kivy.uix.widget import Widget
 
 output_file = open('output.txt', 'w')
 
+path = ('\\src\\input-dir\\')
+full_path = (os.getcwd() + path)
+file_list = os.listdir(full_path)
+
+names = []
+tempNames = []
+
+for files in file_list:
+    if files.endswith('.txt'):
+        with open(full_path + files, 'r') as f:
+            line = f.readline()
+            while line:
+                line = line.replace('\n', '')
+                names.append(line)
+                output_file.write(line)
+                line = f.readline()
+                output_file.write('\n')
+
 class Combine_txt_GridLayout(GridLayout):
     pass
 
